54a9d3fee695d8bff7753791969d3d0521a6b672
Johan Lindfors <johan.lindfors@coderox.se>
2019-02-14 16:17:26 +0100

    Now using pure python approach thanks to Kirill

--- scripts/cppwinrt.py
+++ scripts/cppwinrt.py
@@ -1,18 +1,30 @@
 import argparse
+import os
 import xml.etree.ElementTree as ET
 
-#parser = argparse.ArgumentParser(description='inject_nuget_xml')
-#parser.add_argument('file')
-#parser.add_argument('version')
+parser = argparse.ArgumentParser(description='inject_nuget_xml')
+parser.add_argument('file')
+parser.add_argument('version')
 
-#args = parser.parse_args()
+args = parser.parse_args()
 
-def inject_nuget_xml(file, version):
+def fix_project_file(file, cppwinrt_version):
     ET.register_namespace("", "http://schemas.microsoft.com/developer/msbuild/2003")
     tree = ET.parse(file)
     root = tree.getroot()
 
-    package = "packages\\Microsoft.Windows.CppWinRT." + version + "\\build\\native\\Microsoft.Windows.CppWinRT"
+    for definition in root.iter('{http://schemas.microsoft.com/developer/msbuild/2003}ItemDefinitionGroup'):
+        for midl in definition.iter('{http://schemas.microsoft.com/developer/msbuild/2003}Midl'):
+            definition.remove(midl)
+
+    for item_group in root.iter('{http://schemas.microsoft.com/developer/msbuild/2003}ItemGroup'):
+        for included_file in item_group.iter('{http://schemas.microsoft.com/developer/msbuild/2003}ClInclude'):
+            xaml_file = included_file.attrib['Include'].replace('.h', '.xaml')
+            if os.path.isfile(xaml_file):
+                dependency = ET.SubElement(included_file, "DependentUpon")
+                dependency.text = xaml_file
+
+    package = "packages\\Microsoft.Windows.CppWinRT." + cppwinrt_version + "\\build\\native\\Microsoft.Windows.CppWinRT"
     props =  package + ".props"
     target = package + ".targets"
 
@@ -43,4 +55,4 @@ def inject_nuget_xml(file, version):
 
     tree.write(file, xml_declaration=True, encoding='utf-8')
 
-#inject_nuget_xml(args.file, args.version)
+fix_project_file(args.file, args.version)
