25bbb71fb392da682e6e42351c3544606d3119af
Johan Lindfors <johan.lindfors@coderox.se>
2019-02-15 13:11:36 +0100

    Now parsing version number from packages.config

--- scripts/cppwinrt.py
+++ scripts/cppwinrt.py
@@ -3,8 +3,8 @@ import os
 import xml.etree.ElementTree as ET
 
 parser = argparse.ArgumentParser(description='inject_nuget_xml')
-parser.add_argument('file')
-parser.add_argument('version')
+parser.add_argument('path')
+parser.add_argument('project_name')
 
 args = parser.parse_args()
 
@@ -13,10 +13,12 @@ def fix_project_file(file, cppwinrt_version):
     tree = ET.parse(file)
     root = tree.getroot()
 
+    # Remove MIDL section (which will be populated by nuget package props)
     for definition in root.iter('{http://schemas.microsoft.com/developer/msbuild/2003}ItemDefinitionGroup'):
         for midl in definition.iter('{http://schemas.microsoft.com/developer/msbuild/2003}Midl'):
             definition.remove(midl)
 
+    # Add DependentUpon if .xaml file exists, leveraged by C++/WinRT metadata generation
     for item_group in root.iter('{http://schemas.microsoft.com/developer/msbuild/2003}ItemGroup'):
         for included_file in item_group.iter('{http://schemas.microsoft.com/developer/msbuild/2003}ClInclude'):
             xaml_file = included_file.attrib['Include'].replace('.h', '.xaml')
@@ -55,4 +57,15 @@ def fix_project_file(file, cppwinrt_version):
 
     tree.write(file, xml_declaration=True, encoding='utf-8')
 
-fix_project_file(args.file, args.version)
+def find_cppwinrt_version(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    for packages in root.iter('packages'):
+        for package in packages.iter('package'):
+            if(package.attrib['id'] == 'Microsoft.Windows.CppWinRT'):
+                return package.attrib['version']
+
+version = find_cppwinrt_version(args.path + '\packages.config')
+project_file = args.path + args.project_name + '.vcxproj'
+
+fix_project_file(project_file, version)
